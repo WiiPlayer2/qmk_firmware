34b40d9f793f4a95936a521d373e5ba28ca58289
Waldemar Tomme <waldemar.tomme@bluehands.de>
2023-10-26 16:50:42 +0200

    Add DE_PLUS key with all its modded variants

--- keyboards/wiiplayer2/customized_split/keymaps/personal/keymap.c
+++ keyboards/wiiplayer2/customized_split/keymaps/personal/keymap.c
@@ -14,7 +14,7 @@
 #include "sendstring_german.h"
 
 #define LY_NAV 1
-#define LY_BRA 2
+#define LY_DEV 2
 #define LY_NUM 3
 #define LY_CFG 4
 
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LEFT_SHIFT,  KC_A,       KC_S,       KC_D,       KC_F,                   KC_G,               KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
         KC_LEFT_CTRL,   KC_Z,       KC_X,       KC_C,       KC_V,                   KC_B,               KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_NUHS,
                                                 KC_TAB,     KC_HOME,                KC_RALT,            KC_RGUI,    KC_END,     KC_LALT,
-                                                MO(LY_NAV), LT(LY_BRA,KC_SPACE),    KC_BACKSPACE,       KC_DEL,     KC_ENTER,   MO(LY_NUM)
+                                                MO(LY_NAV), LT(LY_DEV,KC_SPACE),    KC_BACKSPACE,       KC_DEL,     KC_ENTER,   MO(LY_NUM)
     ),
     [LY_NAV] = LAYOUT(
         KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,              KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,
@@ -43,13 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______,    _______,    _______,            _______,    _______,    _______,
                                             _______,    MO(LY_CFG), _______,            _______,    _______,    MY_QCKAC
     ),
-    [LY_BRA] = LAYOUT(
-        _______,    _______,        _______,    _______,        _______,    _______,            _______,    _______,    _______,    _______,    _______,    _______,
-        _______,    RALT(KC_7),     KC_ASTR,    KC_LPRN,        RALT(KC_0), M_BCKTCK,            _______,    _______,    _______,    _______,    _______,    _______,
-        _______,    RALT(KC_8),     KC_NUBS,    LSFT(KC_NUBS),  RALT(KC_9), DE_CIRC,            _______,    _______,    _______,    _______,    _______,    _______,
-        _______,    RALT(KC_NUBS),  KC_AMPR,    RALT(KC_MINS),  KC_BSLS,    M_LAMBDA,           _______,    _______,    _______,    _______,    _______,    _______,
-                                                _______,        _______,    _______,            _______,    _______,    _______,
-                                                _______,        _______,    _______,            _______,    _______,    _______
+    [LY_DEV] = LAYOUT(
+        _______,    _______,        _______,    _______,        _______,    _______,            _______,        _______,    _______,    _______,    _______,    _______,
+        _______,    RALT(KC_7),     KC_ASTR,    KC_LPRN,        RALT(KC_0), M_BCKTCK,           RALT(DE_PLUS),  _______,    _______,    _______,    _______,    _______,
+        _______,    RALT(KC_8),     KC_NUBS,    LSFT(KC_NUBS),  RALT(KC_9), DE_CIRC,            LSFT(DE_PLUS),  _______,    _______,    _______,    _______,    _______,
+        _______,    RALT(KC_NUBS),  KC_AMPR,    RALT(KC_MINS),  KC_BSLS,    M_LAMBDA,           DE_PLUS,        _______,    _______,    _______,    _______,    _______,
+                                                _______,        _______,    _______,            _______,        _______,    _______,
+                                                _______,        _______,    _______,            _______,        _______,    _______
     ),
     [LY_NUM] = LAYOUT(
         _______,    _______,    _______,    _______,    _______,    _______,            KC_NUM,     KC_P7,      KC_P8,      KC_P9,      KC_PSLS,    _______,
